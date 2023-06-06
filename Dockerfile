82d79c04daad88ddb99d3175e251b4824f20115a
liam <huyhomie66@gmail.com>
2023-06-07 00:43:16 +0700

    build project gen cms

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:18.16.0
+#FROM strapi/base
+
+WORKDIR usr/src/app
+
+COPY ./package*.json ./
+
+RUN npm install -g npm@9.5.1
+
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+EXPOSE 1337
+
+CMD ["npm","run","develop"]
---
+++ config/middlewares.ts
@@ -0,0 +1,40 @@
+export default ({ env }) => ([
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'market-assets.strapi.io',
+            `${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`,
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'market-assets.strapi.io',
+            `${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+]);
