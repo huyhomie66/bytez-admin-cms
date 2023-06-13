e053cd9c6b6d249b2a8abaee5384766579ab95e4
liam <huyhomie66@gmail.com>
2023-06-14 00:35:54 +0700

    add component for How It Works page

--- dist/src/api/homepage/controllers/homepage.js
+++ dist/src/api/homepage/controllers/homepage.js
@@ -1,7 +1,7 @@
 "use strict";
 /**
- * content service
+ * homepage controller
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreService('api::content.content');
+exports.default = strapi_1.factories.createCoreController('api::homepage.homepage');
--- dist/src/api/homepage/routes/homepage.js
+++ dist/src/api/homepage/routes/homepage.js
@@ -1,7 +1,7 @@
 "use strict";
 /**
- * content router
+ * homepage router
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreRouter('api::content.content');
+exports.default = strapi_1.factories.createCoreRouter('api::homepage.homepage');
