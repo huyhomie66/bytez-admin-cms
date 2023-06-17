dc2bbb72ee14cd6eb8cdb11e96803377004d0cb2
liam <huyhomie66@gmail.com>
2023-06-17 16:28:47 +0700

    add field image for user

---
+++ src/api/how-it-works-parent/routes/how-it-works-parent.ts
@@ -0,0 +1,7 @@
+/**
+ * how-it-works-parent router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::how-it-works-parent.how-it-works-parent');
---
+++ src/api/how-it-works-parent/services/how-it-works-parent.ts
@@ -0,0 +1,7 @@
+/**
+ * how-it-works-parent service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::how-it-works-parent.how-it-works-parent');
