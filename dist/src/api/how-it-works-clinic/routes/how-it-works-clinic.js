bda7fb60661525686db171146558c6b9bdc948e0
liam <huyhomie66@gmail.com>
2023-06-17 17:49:22 +0700

    add page blog

---
+++ dist/src/api/how-it-works-clinic/routes/how-it-works-clinic.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * how-it-works-clinic router
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreRouter('api::how-it-works-clinic.how-it-works-clinic');
---
+++ src/api/how-it-works-clinic/services/how-it-works-clinic.ts
@@ -0,0 +1,7 @@
+/**
+ * how-it-works-clinic service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::how-it-works-clinic.how-it-works-clinic');
