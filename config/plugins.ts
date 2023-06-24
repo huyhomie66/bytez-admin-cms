98b0e64af4a85980a2b25f8e374860d9acf5061c
liam <huyhomie66@gmail.com>
2023-06-24 18:22:51 +0700

    update swagger

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,13 +1,51 @@
 module.exports = ({ env }) => ({
-    upload: {
-        provider: 'aws-s3',
-        providerOptions: {
-            accessKeyId: env('AWS_ACCESS_KEY_ID'),
-            secretAccessKey: env('AWS_ACCESS_SECRET'),
-            region: env('AWS_REGION'),
-            params: {
-                Bucket: env('AWS_BUCKET'),
-            },
+  documentation: {
+    enabled: true,
+    config: {
+      openapi: "3.0.0",
+      info: {
+        version: "1.0.0",
+        title: "DOCUMENTATION",
+        description: "",
+        termsOfService: "YOUR_TERMS_OF_SERVICE_URL",
+        contact: {
+          name: "TEAM",
+          email: "contact-email@something.io",
+          url: "mywebsite.io",
         },
+        license: {
+          name: "Apache 2.0",
+          url: "https://www.apache.org/licenses/LICENSE-2.0.html",
+        },
+      },
+      "x-strapi-config": {
+        // Leave empty to ignore plugins during generation
+        plugins: ["upload", "users-permissions"],
+        path: "/documentation",
+      },
+      servers: [
+        { url: "http://localhost:1337/api", description: "Development server" },
+        {
+          url: "https://gen-dev.bytez.pro/api",
+          description: "Staging server",
+        },
+      ],
+      externalDocs: {
+        description: "Find out more",
+        url: "https://docs.strapi.io/developer-docs/latest/getting-started/introduction.html",
+      },
+      security: [{ bearerAuth: [] }],
     },
+  },
+  upload: {
+    provider: "aws-s3",
+    providerOptions: {
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      region: env("AWS_REGION"),
+      params: {
+        Bucket: env("AWS_BUCKET"),
+      },
+    },
+  },
 });
