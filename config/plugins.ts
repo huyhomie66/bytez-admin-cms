234ac3dae701fbaa545719822ae1440c5428fe96
liam <huyhomie66@gmail.com>
2023-07-03 02:57:25 +0700

    config s3

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,47 +1,47 @@
 module.exports = ({ env }) => ({
-  // documentation: {
-  //   enabled: true,
-  //   config: {
-  //     openapi: "3.0.0",
-  //     info: {
-  //       version: "1.0.0",
-  //       title: "DOCUMENTATION",
-  //       description: "",
-  //       termsOfService: "YOUR_TERMS_OF_SERVICE_URL",
-  //       contact: {
-  //         name: "TEAM",
-  //         email: "contact-email@something.io",
-  //         url: "mywebsite.io",
-  //       },
-  //       license: {
-  //         name: "Apache 2.0",
-  //         url: "https://www.apache.org/licenses/LICENSE-2.0.html",
-  //       },
-  //     },
-  //     "x-strapi-config": {
-  //       // Leave empty to ignore plugins during generation
-  //       plugins: ["upload", "users-permissions"],
-  //       path: "/documentation",
-  //     },
-  //     servers: [
-  //       { url: "http://localhost:1337/api", description: "Development server" },
-  //       {
-  //         url: "https://gen-dev.bytez.pro/api",
-  //         description: "Staging server",
-  //       },
-  //     ],
-  //     externalDocs: {
-  //       description: "Find out more",
-  //       url: "https://docs.strapi.io/developer-docs/latest/getting-started/introduction.html",
-  //     },
-  //     security: [{ bearerAuth: [] }],
-  //   },
-  // },
+  documentation: {
+    enabled: true,
+    config: {
+      openapi: "3.0.0",
+      info: {
+        version: "1.0.0",
+        title: "DOCUMENTATION",
+        description: "",
+        termsOfService: "YOUR_TERMS_OF_SERVICE_URL",
+        contact: {
+          name: "TEAM",
+          email: "contact-email@something.io",
+          url: "mywebsite.io",
+        },
+        license: {
+          name: "Apache 2.0",
+          url: "https://www.apache.org/licenses/LICENSE-2.0.html",
+        },
+      },
+      "x-strapi-config": {
+        // Leave empty to ignore plugins during generation
+        plugins: ["upload", "users-permissions"],
+        path: "/documentation",
+      },
+      servers: [
+        { url: "http://localhost:1337/api", description: "Development server" },
+        {
+          url: "https://gen-dev.bytez.pro/api",
+          description: "Staging server",
+        },
+      ],
+      externalDocs: {
+        description: "Find out more",
+        url: "https://docs.strapi.io/developer-docs/latest/getting-started/introduction.html",
+      },
+      security: [{ bearerAuth: [] }],
+    },
+  },
   upload: {
     provider: "aws-s3",
     providerOptions: {
-      accessKeyId:'AKIAQQQQVFVHTDOMBX5P',
-      secretAccessKey: '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',
+      accessKeyId:'AKIAQQQQVFVHYQ2DB2RZ',
+      secretAccessKey: '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',
       region: 'us-east-1',
       params: {
         Bucket: 'gen-cms',
